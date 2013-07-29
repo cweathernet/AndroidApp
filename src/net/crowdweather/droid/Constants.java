8d5eebd29ef940dc443a7b253ba0db3f30ae2279
pahan <pahan123@gmail.com>
2013-07-29 23:19:14 +0530

    can submit dummy data

---
+++ src/net/crowdweather/droid/Constants.java
@@ -0,0 +1,6 @@
+package net.crowdweather.droid;
+
+public class Constants {
+public static String API_BASE_URL = "http://10.0.2.2";
+
+}
--- src/net/crowdweather/droid/MainActivity.java
+++ src/net/crowdweather/droid/MainActivity.java
@@ -1,8 +1,26 @@
 package net.crowdweather.droid;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+// remove openintents imports for running real device
 
 public class MainActivity extends Activity {
 
@@ -10,6 +28,55 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final Button button = (Button) findViewById(R.id.publish);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				new getinternetData().execute();
+
+			}
+		});
+	}
+
+	class getinternetData extends AsyncTask<Void, Void, String> {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(Constants.API_BASE_URL);
+			try {
+				// Add your data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+						2);
+				nameValuePairs.add(new BasicNameValuePair("temperature", "30"));
+				nameValuePairs.add(new BasicNameValuePair("humidity",
+						"80%"));
+				nameValuePairs.add(new BasicNameValuePair("pressure",
+						"80%"));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost);
+
+				// message.setText("444");
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200) {
+					return "connect success";
+				} else
+					return "Couldn't connect";
+			} catch (Exception e) {
+				return e.toString();
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			if (result != null) {
+				Toast.makeText(MainActivity.this, "response  : " + result,
+						Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 
 	@Override
