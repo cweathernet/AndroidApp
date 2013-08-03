6cf7fcda0a03cf1cf30e9bc69f8fc245a28d0d3d
pahan <pahan123@gmail.com>
2013-08-03 22:42:12 +0530

    weather service connected

--- src/net/crowdweather/droid/Constants.java
+++ src/net/crowdweather/droid/Constants.java
@@ -1,6 +1,9 @@
 package net.crowdweather.droid;
 
 public class Constants {
-public static String API_BASE_URL = "http://192.168.1.6";
+public static final String API_BASE_URL = "http://82.196.11.207:27080";
+public static final String INSERT_URL = API_BASE_URL+"/weather/items/_insert";
+public static final String GET_URL = API_BASE_URL+"/weather/items/_insert";
+
 
 }
--- src/net/crowdweather/droid/MainActivity.java
+++ src/net/crowdweather/droid/MainActivity.java
@@ -1,7 +1,10 @@
 package net.crowdweather.droid;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -11,10 +14,15 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +33,8 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private GPSTracker gps;
+	private double latitude;
+	private double longitude;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +45,14 @@ public class MainActivity extends Activity {
 
 		if (gps.canGetLocation()) {
 
-			double latitude = gps.getLatitude();
-			double longitude = gps.getLongitude();
-
+			latitude = gps.getLatitude();
+			longitude = gps.getLongitude();
 			// \n is for new line
 			Toast.makeText(
 					getApplicationContext(),
 					"Your Location is - \nLat: " + latitude + "\nLong: "
 							+ longitude, Toast.LENGTH_LONG).show();
+
 		} else {
 			// can't get location
 			// GPS or Network is not enabled
@@ -55,32 +65,53 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				new getinternetData().execute();
 
+				// getCityFromLocation(37.42273,-122.084198);
+
 			}
 		});
 	}
 
 	class getinternetData extends AsyncTask<Void, Void, String> {
+		
+
+		private static final String TAG = "mainACT";
 
 		@Override
 		protected String doInBackground(Void... params) {
 
 			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(Constants.API_BASE_URL);
+			HttpPost httppost = new HttpPost(Constants.INSERT_URL);
 			try {
 				// Add your data
 				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
-						2);
-				nameValuePairs.add(new BasicNameValuePair("temperature", "30"));
-				nameValuePairs.add(new BasicNameValuePair("humidity", "80%"));
-				nameValuePairs.add(new BasicNameValuePair("pressure", "80%"));
+						1);
+				
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("temperature", "30");
+				jsonObj.put("humidity", "80%");
+				jsonObj.put("pressure", "80%");
+				JSONArray locArr  = new JSONArray();
+				locArr.put(latitude);
+				locArr.put(longitude);
+				
+//				jsonObj.put("longitude", longitude);
+//				jsonObj.put("latitude", latitude);
+				jsonObj.put("location",locArr);
+				jsonObj.put("date", Calendar.getInstance(Locale.getDefault()).getTime().toString());
+				
+				JSONArray jsonArry =new  JSONArray();
+				jsonArry.put(jsonObj);
+				
+				nameValuePairs.add(new BasicNameValuePair("docs", jsonArry.toString()));
+				
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
+				Log.d(TAG, httppost.getEntity().getContent().toString());
 				// Execute HTTP Post Request
 				HttpResponse response = httpclient.execute(httppost);
 
-				// message.setText("444");
 				StatusLine statusLine = response.getStatusLine();
 				int statusCode = statusLine.getStatusCode();
+
 				if (statusCode == 200) {
 					return "connect success";
 				} else
@@ -104,6 +135,27 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void getCityFromLocation(double latitude, double longitude) {
+// TODO: integrate google maps api reverse GEO coding.
+		try {
+			Geocoder c = new Geocoder(this, Locale.getDefault());
+			List<Address> l = c.getFromLocation(latitude, longitude, 5);
+
+			for (Address a : l) {
+
+				Log.d("GeocoderTest", "Locality" + a.getLocality()
+						+ "Postal Code " + a.getPostalCode() + " (" + a + ")");
+				
+				// return a.getLocality();
+			}
+
+		} catch (IOException e) {
+			Log.d("GeocoderTest", "error");
+			Log.e("GeocoderTest", "", e);
+		}
+
 	}
 
 }
